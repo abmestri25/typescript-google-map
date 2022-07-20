0b56c05dd05bc825da0f3c73e25ec5b447010578
Abhishek <abmestri25@gmail.com>
2022-07-20 18:18:44 +0530

    Refact : Removed API KEY

---
+++ src/app.ts
@@ -0,0 +1,35 @@
+import axios from 'axios'
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+const GOOGLE_API_KEY = "YOUR GOOGLE API KEY"
+// declare var google: any;
+type GoogleGeoCodingResponse = {
+    results: { geometry: { location: { lat: number, lng: number } } }[];
+    status: 'OK' | 'ZERO_RESULTS';
+}
+
+function searchAddressHandler(event: Event) {
+    event.preventDefault();
+    const enteredAddress = addressInput.value;
+    axios.get<GoogleGeoCodingResponse>(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`)
+        .then((res) => {
+            if (res.data.status !== 'OK') {
+                throw new Error('Could not fetch location !')
+            }
+            const coordinates = res.data.results[0].geometry.location
+            const map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+                center: coordinates,
+                zoom: 16,
+            });
+
+            new google.maps.Marker({
+                position: coordinates,
+                map: map,
+            });
+        })
+        .catch((err) => console.log(err));
+
+}
+
+form?.addEventListener('submit', searchAddressHandler)
